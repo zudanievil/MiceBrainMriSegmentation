128e7e2a0f7931c708e75851a62a0ca289131195
Zuev Daniil <zudanievil@gmail.com>
2020-12-15 10:52:57 +0700

    fixed summary plotting

--- scripts/plot_segmentation_summary.py
+++ scripts/plot_segmentation_summary.py
@@ -17,5 +17,5 @@ if __name__ == '__main__':
     projects = pathlib.Path('c:/users/user/desktop/new_segm/')
     for p in ['cgtg_cgtg', 'cgtg_c57bl']:
         project = projects / p
-        # segmentation_by_atlas.refactor_summary(project/'results_left.txt', project/'results_left_refactored.txt')
+        segmentation_by_atlas.refactor_summary(project/'results_left.txt', project/'results_left_refactored.txt')
         segmentation_by_atlas.plot_segmentation_results(project, project/'results_left_refactored.txt')
